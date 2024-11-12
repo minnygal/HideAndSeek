3a4571eec2f0573ecde2c32d6e1ce9ae8c9fcc7a
Heather <minny@minnystuff.com>
2024-11-12 11:24:52 -0500

    funct: add extension method to Direction enum to get opposite direction

--- Direction.cs
+++ Direction.cs
@@ -15,4 +15,17 @@
         In = -6,
         Out = 6
     }
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// Get the opposite Direction of the direction provided
+        /// </summary>
+        /// <param name="direction">Direction to find the opposite of</param>
+        /// <returns>The opposite Direction of the direction provided</returns>
+        public static Direction OppositeDirection(this Direction direction)
+        {
+            return (Direction)((int)(direction) * -1); // direction provided as integer multiplied by -1 and converted back to Direction
+        }
+    }
 }
