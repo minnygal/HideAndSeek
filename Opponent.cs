7a0886acd86f8ab7d535776cb09557a916559912
minnygal <minny@minnystuff.com>
2025-01-02 14:28:42 -0500

    add: implement Hide method in Opponent class to hide opponent in random location within the House

--- Opponent.cs
+++ Opponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,29 @@ namespace HideAndSeek
 
         public void Hide()
         {
-            throw new NotImplementedException();
+            // Current location of opponent moving through House to find hiding place
+            Location currentLocation = House.Entry;
+
+            // Minimum number of moves opponent makes through the house to find a hiding spot
+            int minNumberOfMoves = House.Random.Next(10, 51);
+
+            // Move through house via random exits between 10 and 50 times (inclusive)
+            for(int currentMove = 0; currentMove < minNumberOfMoves; currentMove++)
+            {
+               currentLocation = House.RandomExit(currentLocation);
+            }
+
+            // Keep moving until a location with hiding place is found
+            while(currentLocation.GetType() != typeof(LocationWithHidingPlace))
+            {
+                currentLocation = House.RandomExit(currentLocation);
+            }
+
+            // Initialize hiding place to current location
+            LocationWithHidingPlace hidingPlace = currentLocation as LocationWithHidingPlace;
+
+            // Hide in hiding place
+            hidingPlace.Hide(this);
         }
     }
 }
