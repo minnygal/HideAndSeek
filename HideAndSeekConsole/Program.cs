c0655cef58a12cbaa3f84a7e4567e0aaa81fda76
minnygal <minny@minnystuff.com>
2025-04-03 15:15:17 -0400

    update: Program class to allow user to enter House layout file name and run game

--- HideAndSeekConsole/Program.cs
+++ HideAndSeekConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HideAndSeek
+﻿using System.Xml.Linq;
+
+namespace HideAndSeek
 {
     /// <summary>
     /// Class to run in Console interactive game of HideAndSeek
@@ -18,6 +20,7 @@
      * -I added instructions to be printed for the user when they launch the program.
      * -I used the RestartGame method I added to GameController so
      *  GameController only has to be created once.
+     * -I allow the user to enter a House layout name or use the default House layout.
      * -I added comments to make the code more readable.
      * -I made code conform to my formatting.
      * -I added code to print an empty line to put space between moves information.
@@ -28,22 +31,52 @@
         {
             // Print basic instructions
             Console.WriteLine("Welcome to the Hide And Seek Console App!");
-            Console.WriteLine("-Navigate through rooms in a virtual house to find hiding opponents.");
-            Console.WriteLine("-Find all the opponents to win the game.");
-            Console.WriteLine("-The goal is to win the game in the fewest number of moves possible.");
+            Console.WriteLine("-Navigate through rooms in a virtual house to find all the hiding opponents " +
+                              "in the fewest number of moves possible.");
             Console.WriteLine("-To MOVE, enter the direction in which you want to move.");
             Console.WriteLine("-To CHECK if any opponents are hiding in your current location, enter \"check\".");
             Console.WriteLine("-To SAVE your progress, enter \"save\" followed by a space and a name for your game.");
             Console.WriteLine("-To LOAD a saved game, enter \"load\" followed by a space and the name of your game.");
             Console.WriteLine("-To DELETE a saved game, enter \"delete\" followed by a space and the name of your game.");
-            Console.WriteLine(); // Print empty line to put space between basic instructions and game
+            Console.WriteLine();
 
-            // Create new game controller (also starts new game)
+            // Create new game controller
             GameController gameController = new GameController();
 
             // Until the user quits the program
             while (true)
             {
+                bool houseLayoutChosen = false; // Set flag
+                do
+                {
+                    // Get House file name
+                    Console.Write("Type a house layout file name or just press Enter to use the default house layout: "); // Prompt for House layout file name
+                    string houseLayoutFileName = Console.ReadLine(); // Get user input
+
+                    try
+                    {
+                        // If no House file name was entered
+                        if (houseLayoutFileName.Trim() == "")
+                        {
+                            gameController.RestartGame("DefaultHouse"); // Restart game
+                        }
+                        else // If a House file name was entered
+                        {
+                            gameController.RestartGame(houseLayoutFileName); // Restart game with House layout file
+                        }
+
+                        // Update flag
+                        houseLayoutChosen = true;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message); // Print exception message
+                    }
+                } while (!(houseLayoutChosen));
+                
+                // Welcome player to the House
+                Console.WriteLine($"{Environment.NewLine}Welcome to {gameController.House.Name}!");
+
                 // Until game is over
                 while ( !(gameController.GameOver) )
                 {
@@ -62,9 +95,6 @@
                 {
                     return;
                 }
-
-                // Otherwise, restart game
-                gameController.RestartGame();
             }
         }
     }
