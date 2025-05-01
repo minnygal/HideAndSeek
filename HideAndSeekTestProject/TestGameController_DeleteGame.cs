392ab6ccbd581eb59be7621c9ae11001d7249244
minnygal <minny@minnystuff.com>
2025-05-01 12:00:40 -0400

    refactor: GameController DeleteGame tests to use GameController constructor with House object, and improve test code

--- HideAndSeekTestProject/TestGameController_DeleteGame.cs
+++ HideAndSeekTestProject/TestGameController_DeleteGame.cs
@@ -10,31 +10,37 @@ namespace HideAndSeek
 {
     /// <summary>
     /// GameController tests for DeleteGame method to delete saved game file
-    /// (creates new GameController with unmocked House, Location, and LocationWithHidingPlace objects,
-    ///  but does not test these objects)
+    /// 
+    /// These are integration tests using House, Location, and LocationWithHidingPlace
+    /// although these classes are not tested
     /// </summary>
     [TestFixture]
     public class TestGameController_DeleteGame
     {
         private GameController gameController;
-        private string message; // Message returned by LoadGame
-        private Exception exception; // Exception thrown by LoadGame
+        private House house; // House object passed in to GameController constructor
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // Set House variable
+            Location entryLocation = new Location("Entry"); // Create entry
+            LocationWithHidingPlace locationWithHidingPlace = entryLocation.AddExit(Direction.East, "Office", "under the table"); // Add new connecting location with hiding place
+            house = new House("test house", "TestHouse", "Entry",
+                              new List<Location>() { entryLocation },
+                              new List<LocationWithHidingPlace>() { locationWithHidingPlace }); // Create House
+        }
 
         [SetUp]
         public void Setup()
         {
-            gameController = new GameController(new Opponent[] { new Mock<Opponent>().Object }, "DefaultHouse"); // Create new GameController with mocked Opponent and default House
-            message = null;
-            exception = null;
-            House.FileSystem = MockFileSystemHelper.GetMockedFileSystem_ToReadAllText(
-                               "DefaultHouse.house.json", DefaultHouse_Serialized); // Set mock file system for House property to return default House file text
+            gameController = new GameController(new Opponent[] { new Mock<Opponent>().Object }, house); // Create GameController with mocked Opponent and House
             GameController.FileSystem = new FileSystem(); // Set static GameController file system to new file system
         }
 
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
-            House.FileSystem = new FileSystem(); // Set static House file system to new file system
             GameController.FileSystem = new FileSystem(); // Set static GameController file system to new file system
         }
 
@@ -43,12 +49,18 @@ namespace HideAndSeek
         public void Test_GameController_DeleteGame_AndCheckErrorMessage_ForInvalidFileName(
             [Values("", " ", "my saved game", "my\\saved\\game", "my/saved/game", "my/saved\\ game")] string fileName)
         {
-            
-            exception = Assert.Throws<ArgumentException>(() => gameController.DeleteGame(fileName));
-            Assert.That(exception.Message, Does.StartWith($"Cannot perform action because file name \"{fileName}\" " +
-                                                       "is invalid (is empty or contains illegal characters, e.g. \\, /, or whitespace)"));
-            Assert.That(exception.Message, Does.StartWith($"Cannot perform action because file name \"{fileName}\" " +
-                                                           "is invalid (is empty or contains illegal characters, e.g. \\, /, or whitespace)"));
+            Assert.Multiple(() =>
+            {
+                // Assert that attempting to delete game with invalid file name raises exception
+                Exception exception = Assert.Throws<ArgumentException>(() =>
+                {
+                    gameController.DeleteGame(fileName);
+                });
+
+                // Assert that exception message is as expected
+                Assert.That(exception.Message, Does.StartWith($"Cannot perform action because file name \"{fileName}\" " +
+                                                               "is invalid (is empty or contains illegal characters, e.g. \\, /, or whitespace)"));
+            });
         }
 
         [Test]
@@ -60,11 +72,17 @@ namespace HideAndSeek
             mockFileSystemForGameController.Setup(manager => manager.File.Exists("my_nonexistent.game.json")).Returns(false); // Mock that file does not exist
             GameController.FileSystem = mockFileSystemForGameController.Object;
 
-            // Assert that attempt to delete nonexistent game raises exception
-            exception = Assert.Throws<FileNotFoundException>(() => gameController.DeleteGame("my_nonexistent_game"));
+            Assert.Multiple(() =>
+            {
+                // Assert that attempting to delete nonexistent game raises exception
+                Exception exception = Assert.Throws<FileNotFoundException>(() =>
+                {
+                    gameController.DeleteGame("my_nonexistent_game");
+                });
 
-            // Assert that error message is as expected
-            Assert.That(exception.Message, Is.EqualTo("Could not delete game because file my_nonexistent_game does not exist"));
+                // Assert that exception message is as expected
+                Assert.That(exception.Message, Is.EqualTo("Could not delete game because file my_nonexistent_game does not exist"));
+            });
         }
 
         [Test]
@@ -76,156 +94,14 @@ namespace HideAndSeek
             mockFileSystemForGameController.Setup(manager => manager.File.Exists("my_saved_game.game.json")).Returns(true); // Mock that file exists
             GameController.FileSystem = mockFileSystemForGameController.Object;
 
-            // Have game controller delete game
-            message = gameController.DeleteGame("my_saved_game");
-
-            // Verify that File.Delete was called once
-            mockFileSystemForGameController.Verify(manager => manager.File.Delete("my_saved_game.game.json"), Times.Once);
-
-            // Assert that success message is as expected
-            Assert.That(message, Is.EqualTo("Game file my_saved_game has been successfully deleted"));
-        }
-
-        /// <summary>
-        /// Text representing default House for tests serialized
-        /// </summary>
-        private static string DefaultHouse_Serialized
-        {
-            get
+            Assert.Multiple(() =>
             {
-                return
-                    "{" +
-                        "\"Name\":\"my house\"" + "," +
-                        "\"HouseFileName\":\"DefaultHouse\"" + "," +
-                        "\"PlayerStartingPoint\":\"Entry\"" + "," +
-                        "\"LocationsWithoutHidingPlaces\":" +
-                        "[" +
-                            "{" +
-                                "\"Name\":\"Hallway\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"West\":\"Entry\"," +
-                                    "\"Northwest\":\"Kitchen\"," +
-                                    "\"North\":\"Bathroom\"," +
-                                    "\"South\":\"Living Room\"," +
-                                    "\"Up\":\"Landing\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"Name\":\"Landing\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"Down\":\"Hallway\"," +
-                                    "\"Up\":\"Attic\"," +
-                                    "\"Southeast\":\"Kids Room\"," +
-                                    "\"Northwest\":\"Master Bedroom\"," +
-                                    "\"Southwest\":\"Nursery\"," +
-                                    "\"South\":\"Pantry\"," +
-                                    "\"West\":\"Second Bathroom\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"Name\":\"Entry\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"Out\":\"Garage\"," +
-                                    "\"East\":\"Hallway\"" +
-                                "}" +
-                            "}" +
-                        "]" + "," +
-                        "\"LocationsWithHidingPlaces\":" +
-                        "[" +
-                            "{" +
-                                "\"HidingPlace\":\"in a trunk\"," +
-                                "\"Name\":\"Attic\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"Down\":\"Landing\"" +
-                                "}" +
-                            "}," +
-                            "{\"HidingPlace\":\"behind the door\"," +
-                                "\"Name\":\"Bathroom\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"South\":\"Hallway\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"in the bunk beds\"," +
-                                "\"Name\":\"Kids Room\"," +
-                                "\"ExitsForSerialization\":" +
-                                    "{" +
-                                        "\"Northwest\":\"Landing\"" +
-                                    "}" +
-                                "}," +
-                            "{" +
-                                "\"HidingPlace\":\"under the bed\"," +
-                                "\"Name\":\"Master Bedroom\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"Southeast\":\"Landing\"," +
-                                    "\"East\":\"Master Bath\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"behind the changing table\"," +
-                                "\"Name\":\"Nursery\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"Northeast\":\"Landing\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"inside a cabinet\"," +
-                                "\"Name\":\"Pantry\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"North\":\"Landing\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"in the shower\"," +
-                                "\"Name\":\"Second Bathroom\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"East\":\"Landing\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"next to the stove\"," +
-                                "\"Name\":\"Kitchen\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"Southeast\":\"Hallway\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"in the tub\"," +
-                                "\"Name\":\"Master Bath\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"West\":\"Master Bedroom\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"behind the car\"," +
-                                "\"Name\":\"Garage\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"In\":\"Entry\"" +
-                                "}" +
-                            "}," +
-                            "{" +
-                                "\"HidingPlace\":\"behind the sofa\"," +
-                                "\"Name\":\"Living Room\"," +
-                                "\"ExitsForSerialization\":" +
-                                "{" +
-                                    "\"North\":\"Hallway\"" +
-                                "}" +
-                            "}" +
-                        "]" +
-                    "}";
-            }
+                // Delete game and assert that return message is as expected
+                Assert.That(gameController.DeleteGame("my_saved_game"), Is.EqualTo("Game file my_saved_game has been successfully deleted"));
+
+                // Verify that File.Delete was called once
+                mockFileSystemForGameController.Verify(manager => manager.File.Delete("my_saved_game.game.json"), Times.Once);
+            });
         }
     }
 }
