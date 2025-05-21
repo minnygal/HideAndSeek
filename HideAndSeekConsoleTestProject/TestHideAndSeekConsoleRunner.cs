19e2d3b4ec03e0284d32032354d81b7016844228
minnygal <minny@minnystuff.com>
2025-05-21 15:49:59 -0400

    add: Console app test project

---
+++ HideAndSeekConsoleTestProject/TestHideAndSeekConsoleRunner.cs
@@ -0,0 +1,37 @@
+using HideAndSeek;
+using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions;
+using Moq;
+using System.Text;
+
+namespace HideAndSeekConsoleTestProject
+{
+    [TestFixture]
+    public class TestHideAndSeekConsoleRunner
+    {
+        HideAndSeekConsoleRunner consoleRunner; // Console runner object to be tested
+        Mock<IGameController> mockGameController; // GameController mock used by HideAndSeekConsoleRunner object
+        StringBuilder sbActualTextDisplayed; // StringBuilder to capture actual text displayed (not including user input)
+        Mock<IConsoleIO> mockConsoleIO; // IConsoleIO mock used by HideAndSeekConsoleRunner object
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialize class variables
+            mockGameController = new Mock<IGameController>();
+            consoleRunner = null;
+            mockConsoleIO = new Mock<IConsoleIO>();
+            sbActualTextDisplayed = new StringBuilder();
+
+            // Set up mock IConsoleIO to write text to StringBuilder
+            mockConsoleIO.Setup((cio) => cio.WriteLine(It.IsAny<string>())).Callback((string message) =>
+            {
+                sbActualTextDisplayed.AppendSafeWithNewLine(message); // Append text written to StringBuilder
+            });
+
+            mockConsoleIO.Setup((cio) => cio.Write(It.IsAny<string>())).Callback((string message) =>
+            {
+                sbActualTextDisplayed.Append(message); // Append text written to StringBuilder
+            });
+        }
+    }
+}
