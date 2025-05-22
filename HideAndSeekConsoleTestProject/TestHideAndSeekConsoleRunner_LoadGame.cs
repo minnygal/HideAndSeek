088024691167f9636a77399a3babb367f4fc2753
minnygal <minny@minnystuff.com>
2025-05-22 11:55:56 -0400

    add: ConsoleRunner tests for successfully loading and deleting saved game with one-line command

---
+++ HideAndSeekConsoleTestProject/TestHideAndSeekConsoleRunner_LoadGame.cs
@@ -0,0 +1,88 @@
+﻿using HideAndSeek;
+using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions;
+using Moq;
+using System.Text;
+
+namespace HideAndSeekConsoleTestProject
+{
+    /// <summary>
+    /// HideAndSeekConsoleRunner tests for command to load game
+    /// </summary>
+    [TestFixture]
+    public class TestHideAndSeekConsoleRunner_LoadGame
+    {
+        HideAndSeekConsoleRunner consoleRunner; // Console runner object to be tested
+        Mock<IGameController> mockGameController; // GameController mock used by HideAndSeekConsoleRunner object
+        StringBuilder sbActualTextDisplayed; // StringBuilder to capture actual text displayed (not including user input)
+        Mock<IConsoleIO> mockConsoleIO; // IConsoleIO mock used by HideAndSeekConsoleRunner object
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Initialize class variables
+            mockGameController = new Mock<IGameController>();
+            consoleRunner = null;
+            mockConsoleIO = new Mock<IConsoleIO>();
+            sbActualTextDisplayed = new StringBuilder();
+
+            // Set up mock IConsoleIO to write text to StringBuilder
+            mockConsoleIO.Setup((cio) => cio.WriteLine(It.IsAny<string>())).Callback((string message) =>
+            {
+                sbActualTextDisplayed.AppendSafeWithNewLine(message); // Append text written to StringBuilder
+            });
+
+            mockConsoleIO.Setup((cio) => cio.WriteLine()).Callback(() =>
+            {
+                sbActualTextDisplayed.Append(Environment.NewLine); // Append new line to StringBuilder
+            });
+
+            mockConsoleIO.Setup((cio) => cio.Write(It.IsAny<string>())).Callback((string message) =>
+            {
+                sbActualTextDisplayed.Append(message); // Append text written to StringBuilder
+            });
+
+
+            // Set up mock GameController to return House name (used in welcome message)
+            mockGameController.SetupGet((gc) => gc.House.Name).Returns("tester house");
+
+            // Set up mock GameController to return Status
+            mockGameController.SetupGet((gc) => gc.Status).Returns("[status]");
+
+            // Set up mock GameController to return Prompt
+            mockGameController.SetupGet((gc) => gc.Prompt).Returns("[prompt]");
+        }
+
+        [Test]
+        [Category("ConsoleRunner Load Success")]
+        public void Test_ConsoleRunner_Load_WithFileNameEntered()
+        {
+            // Set up mock GameController to return message when save
+            mockGameController.Setup((gc) => gc.LoadGame("fileName")).Returns("[load return message]");
+
+            // Set up mock to return user input
+            mockConsoleIO.SetupSequence((cio) => cio.ReadLine())
+                .Returns("load fileName") // Load game
+                .Returns("exit"); // Exit game
+
+            // Create console runner with mocked GameController and IConsoleIO
+            consoleRunner = new HideAndSeekConsoleRunner(mockGameController.Object, mockConsoleIO.Object);
+
+            // Run game
+            consoleRunner.RunGame();
+
+            Assert.Multiple(() =>
+            {
+                // Assert text displayed is as expected
+                Assert.That(sbActualTextDisplayed.ToString(), Does.EndWith(
+                    "[load return message]" + Environment.NewLine +
+                    "Welcome to tester house!" + Environment.NewLine +
+                    Environment.NewLine +
+                    "[status]" + Environment.NewLine +
+                    "[prompt]"));
+
+                // Verify that Load method was called
+                mockGameController.Verify((gc) => gc.LoadGame("fileName"), Times.Once);
+            });
+        }
+    }
+}
